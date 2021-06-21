5c320ff788b509a683377b0edd0ff7592d4cbf2d
eric <rfd3344@gmail.com>
2021-06-21 23:36:13 +1000

    initial app

---
+++ main/afterLoaded.ts
@@ -0,0 +1,17 @@
+
+import fs from 'fs';
+import path from 'path';
+import { app } from 'electron';
+
+
+
+export default function afterLoaded() : void {
+  // console.warn('rfd afterLoaded');
+
+  // const fileName = 'recently-used.md';
+  // fs.writeFile(fileName, 'Lorem Ipsum', () => {
+  //   app.addRecentDocument(path.join(__dirname, fileName))
+  // });
+
+
+}
---
+++ main/macOSProcess.ts
@@ -0,0 +1,11 @@
+
+import fs from 'fs';
+import path from 'path';
+import { app } from 'electron';
+
+
+
+export default function macOSProcess() : void {
+
+
+}
